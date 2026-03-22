cc5dc67f7753a3f4fa521e2278db5552e4740ce7
SeoBYP <jajazxcv@naver.com>
2026-03-22 14:46:20 +0900

    유니티 공굴리기 0322 수업

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,33 +1,42 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody rb;
-
-    float movementX;
-    float movementY;
-
     public float speed = 10;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    public int winCount = 11;
+
+    private Rigidbody _rb;
+
+    private float _movementX;
+    private float _movementY;
+
+    private int _counter = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
+        _counter = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     private void FixedUpdate()
     {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed);
+        Vector3 movement = new Vector3(_movementX, 0.0f, _movementY);
+        _rb.AddForce(movement * speed);
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         Debug.Log("On Move");
         Vector2 movementVector = context.ReadValue<Vector2>();
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        _movementX = movementVector.x;
+        _movementY = movementVector.y;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -35,6 +44,17 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            _counter++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + _counter.ToString();
+        if(_counter >= winCount)
+        {
+            winTextObject.SetActive(true);
         }
     }
 }
